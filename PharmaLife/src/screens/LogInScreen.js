5ace2995367ba701ef943c4af603b86154714dda
MatiasGolonbek <47343179@est.ort.edu.ar>
2023-06-16 17:07:33 -0300

    empezando a diseñar registrarse

--- PharmaLife/src/screens/LogInScreen.js
+++ PharmaLife/src/screens/LogInScreen.js
@@ -1,12 +1,32 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
 const LogInScreen = () => {
   return (
-    <View>
-      <Text>LogIn Home 2</Text>
+    <View style={styles.view}>
+      <Text style={styles.baseText}>
+            ¿Cual es tu rol en la aplicación?
+          </Text>
     </View>
   )
 }
+const styles = StyleSheet.create({
+  view: {
+    backgroundColor: "#EFEFEF",
+    flex: 1,
+    height: "auto",
+    width: "auto",
+  },
+  baseText: {
+    textAlign: 'center',
+    marginTop: 180,
+    marginBottom: 30,
+    color: "#2C4521",
+    fontFamily: 'Crimson Text',
+    fontWeight:"bold",
+    fontSize: 25,
+    lineheight: 24,
+  },
+})
 
 export default LogInScreen;

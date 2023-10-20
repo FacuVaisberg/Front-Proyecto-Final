39aa9528130de87055eb8cfa07b570b5fd3e5cb9
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-10-20 09:06:06 -0300

    avance navbar

---
+++ src/components/RecetaSolicitud.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const RecetaSolicitud = () => {
+  return (
+    <View>
+      <Text>RecetaSolicitud</Text>
+    </View>
+  )
+}
+
+export default RecetaSolicitud
+
+const styles = StyleSheet.create({})
